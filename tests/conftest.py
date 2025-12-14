7ed345af31cbf9c9d8cc3501e84d462e7066f5ff
Isaac C. F. Wong <chunfung.wong@kuleuven.be>
2025-12-14 15:01:10 +0100

    test: Update the module docstring of `conftest.py`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,6 @@
-#   ---------------------------------------------------------------------------------
-#   Copyright (c) Microsoft Corporation. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   ---------------------------------------------------------------------------------
-"""
-This is a configuration file for pytest containing customizations and fixtures.
-
-In VSCode, Code Coverage is recorded in config.xml. Delete this file to reset reporting.
-"""
+"""Configuration file for pytest to add custom markers and fixtures."""
 
 from __future__ import annotations
-
-from typing import List
 
 import pytest
 from _pytest.nodes import Item
