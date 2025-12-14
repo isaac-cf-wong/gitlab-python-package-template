8a632066b056cb080cb28881ca86b3ce59bd5132
Isaac C. F. Wong <chunfung.wong@kuleuven.be>
2025-12-14 14:57:39 +0100

    fix: Change the import path to absolute

--- src/package_name/__main__.py
+++ src/package_name/__main__.py
@@ -1,6 +1,8 @@
+"""Main entry point for package_name."""
+
 from __future__ import annotations
 
 if __name__ == "__main__":
-    from .utils.log import setup_logger
+    from package_name.utils.log import setup_logger
 
     setup_logger(print_version=True)
